b56d4170cb1c18d717e58baab04adeb9aeed7b92
juku <kuenzel.justin@t-online.de>
2019-05-10 00:53:44 +0200

    Add login activity

--- app/src/main/java/com/jukusoft/phabdroid/MainActivity.java
+++ app/src/main/java/com/jukusoft/phabdroid/MainActivity.java
@@ -1,7 +1,10 @@
 package com.jukusoft.phabdroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.jukusoft.phabdroid.activity.login.ui.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //go to login activity
+        Intent k = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(k);
     }
 }
